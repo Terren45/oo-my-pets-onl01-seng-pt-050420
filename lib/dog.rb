b99cfaed0d4f0c1233cf9a659614ffbc2183896e
Terren Culberson <culbersonterren@yahoo.com>
2020-06-09 03:57:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog
   # code goes here
   
-  attr_accessor :owner
+  attr_accessor :owner :mood
   attr_reader :name
   
   def initialize(name, owner)
     @name = name 
     @owner = owner
+    @mood = mood
   end
 end
