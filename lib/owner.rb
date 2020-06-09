43f7ab459fdb72fb073d027239888616cc8d007e
Terren Culberson <culbersonterren@yahoo.com>
2020-06-09 03:22:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,8 +3,12 @@ class Owner
   attr_accessor :human
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @species = human
+    @@all << self
+    
   end
 end
