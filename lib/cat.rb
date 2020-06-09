9da0515801a3037cd23b06a7ace7734ff30a4a20
Terren Culberson <culbersonterren@yahoo.com>
2020-06-09 03:33:35 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,5 +3,10 @@ class Cat
   attr_accessor :mood, :owner
   attr_reader :name
 
-
+def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
 end
